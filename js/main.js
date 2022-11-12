6a3be45fec6a29bb8efda42e12f8383c80c2c8a8
Rafiul-Islam <rafi.cse.ru@gmail.com>
2022-11-12 12:39:04 +0600

    sort the collapsible items css

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    const collapsible = document.querySelectorAll(".collapsible");
+
+    collapsible.forEach((item) => {
+        item.addEventListener("click", function(){
+            this.classList.toggle("collapsible--expanded");
+        })
+    })
+}
